a731e6625daf286170a7fc0f24b6ba8543b5416d
Si K <simon.kusners@gmail.com>
2025-04-10 18:08:16 +0100

    Fixed calendar import errors

--- src/main/java/com/sik/pocf/IcsFileWriter.java
+++ src/main/java/com/sik/pocf/IcsFileWriter.java
@@ -6,24 +6,25 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static com.sik.pocf.core.Constants.*;
 
 public class IcsFileWriter {
 
-    private String version =    "VERSION:1.0\n";
-    private String prodid =     "PRODID://Elara/lofy/tanare/delp/314sum2015//\n";
-    private String calBegin =   "BEGIN:VCALENDAR\n";
-    private String calEnd =     "END:VCALENDAR\n";
-    private String eventBegin = "BEGIN:VEVENT\n";
-    private String eventEnd =   "END:VEVENT\n";
+    private String version =    "VERSION:2.0\r\n";
+    private String prodid =     "PRODID://Elara/lofy/tanare/delp/314sum2015//\r\n";
+    private String calBegin =   "BEGIN:VCALENDAR\r\n";
+    private String calEnd =     "END:VCALENDAR\r\n";
+    private String eventBegin = "BEGIN:VEVENT\r\n";
+    private String eventEnd =   "END:VEVENT\r\n";
 
-    private String uid_fmt = "UID:%s\n";
-    private String dtstamp_fmt = "DTSTAMP:%s\n";
-    private String organizer_fmt = "ORGANISER:%s\n";
-    private String dtstart_fmt = "DTSTART:%s\n";
-    private String dtend_fmt = "DTEND:%s\n";
-    private String summary_fmt = "SUMMARY:%s\n";
+    private String uid_fmt = "UID:%s\r\n";
+    private String dtstamp_fmt = "DTSTAMP:%s\r\n";
+    private String organizer_fmt = "ORGANISER:%s\r\n";
+    private String dtstart_fmt = "DTSTART:%s\r\n";
+    private String dtend_fmt = "DTEND:%s\r\n";
+    private String summary_fmt = "SUMMARY:%s\r\n";
 
 
     public void writeEventsToIcsFile(List<ShipEvent> shipEvents, String icsFile ){
@@ -50,8 +51,8 @@ public class IcsFileWriter {
             for (ShipEvent evt : shipEvents) {
 
                 bw.write(eventBegin);
-                bw.write(String.format(uid_fmt, "simon.kusners@gmail.com"));
-                bw.write(String.format(dtstamp_fmt, LocalDateTime.now()));
+                bw.write(String.format(uid_fmt, UUID.randomUUID()));
+                bw.write(String.format(dtstamp_fmt, LocalDateTime.now().format(ICS_DATE_FMT)));
                 bw.write(String.format(organizer_fmt, "/com/sik/si/kusners/"));
                 bw.write(String.format(dtstart_fmt, evt.getDateTime().format(ICS_DATE_FMT)));
                 bw.write(String.format(dtend_fmt, evt.getDateTime().plusMinutes(15L).format(ICS_DATE_FMT)));
